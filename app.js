1cd79ff9bf76b35bb41d0998310e6a49450075be
Patrick <pat.lempert@gmail.com>
2015-11-15 09:14:30 -0500

    changed callback url

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ passport.deserializeUser(function(obj, done) {
 passport.use(new GitHubStrategy({
     clientID: GITHUB_CLIENT_ID,
     clientSecret: GITHUB_CLIENT_SECRET,
-    callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+    callbackURL: "http://git-chat.azurewebsites.net/auth/github/callback" 
+    //"http://127.0.0.1:3000/auth/github/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     // asynchronous verification, for effect...
